bcb3356d0cefbcad889c5386bed9381490348829
dakodev <sweetdawinner@gmail.com>
2025-02-13 01:53:20 -0300

    feat: enhance UI with navigation bar and logo, update styles for improved layout

--- ticket.js
+++ ticket.js
@@ -10,9 +10,18 @@ let people = {
     "Benz", "Hamilton", "Smith", "Doe", "Lee", "Ramirez",
     "Anderson", "Brown", "Clark", "Johnson", "Martinez",
     "Garcia", "Rodriguez", "Hernandez", "Nguyen", "Taylor",
-    "Wilson", "Davis", "Miller", "Moore"
+    "Wilson", "Davis", "Miller", "Moore","Payne"
   ]
 };
+
+let locations = [
+  "New York", "Los Angeles", "Chicago", "Houston", "Phoenix",
+  "Rio de Janeiro", "Buenos Aires", "London", "Paris", "Tokyo",
+  "Sydney", "Cape Town", "Berlin", "Moscow", "Toronto",
+  "Dubai", "Mumbai", "Beijing", "Seoul", "Mexico City",
+  "São Paulo", "Madrid", "Rome", "Bangkok", "Istanbul"
+];
+  
 
 class Person {
   static passengerCount = 0;
@@ -26,11 +35,24 @@ class Person {
     this.groupNumber = groupNumber;
   }
 
-  info() {
-    console.log(`\x1b[31mPassenger ${this.passengerNumber}\x1b[0m, \x1b[34mGroup ${this.groupNumber}\x1b[0m:
-- Full Name: ${this.name} ${this.surname}
-- Age: ${this.age}
-- Price: ${this.price}`);
+  info(passengersInfo) {
+      let passengerNumber = document.createElement("h4");
+      passengerNumber.innerHTML = `Passenger ${this.passengerNumber}`;
+      passengerNumber.className = `passenger-number`;
+      let passengerFullName = document.createElement("p");
+      passengerFullName.innerHTML = `${this.name} ${this.surname}`;
+      passengerFullName.className = `passenger-name`;
+      let passengerAge = document.createElement("p");
+      passengerAge.innerHTML = `Age: ${this.age}`;
+      let passengerPrice = document.createElement("p");
+      passengerPrice.innerHTML = `Price: $${this.price}`;
+      let passengerInfo = document.createElement("div");
+      passengerInfo.className = `passenger-info`;
+      passengerInfo.appendChild(passengerNumber);
+      passengerInfo.appendChild(passengerFullName);
+      passengerInfo.appendChild(passengerAge);
+      passengerInfo.appendChild(passengerPrice);
+      passengersInfo.appendChild(passengerInfo);
   }
 }
 
@@ -48,29 +70,30 @@ class Group {
     return this.passengers.some((passenger) => passenger.age >= 21);
   }
 
-  groupInfo() {
+  groupInfo(groupContainer) {
     const totalPassengers = this.passengers.length;
     const totalPrice = this.passengers.reduce((sum, p) => sum + p.price, 0);
-    console.log(`\x1b[34mGroup ${this.groupNumber}\x1b[0m:
-- Number of Passengers: ${totalPassengers}
-- Total Price: $${totalPrice}
-`);
+    
     let groupNumber = document.createElement("h3");
     groupNumber.innerHTML = `Group ${this.groupNumber}`;
     let groupTotalPassengers = document.createElement("h4");
-    groupTotalPassengers.innerHTML = `Number of Passengers: ${totalPassengers}`;
+    groupTotalPassengers.innerHTML = `${totalPassengers} passengers total`;
     let groupTotalPrice = document.createElement("h4");
-    groupTotalPrice.innerHTML = `Total Price: $${totalPrice}`;
+    groupTotalPrice.innerHTML = `$${totalPrice}`;
+    groupTotalPrice.className = `price`;
     let groupInfo = document.createElement("div");
     groupInfo.className = `group-info`;
     groupInfo.appendChild(groupNumber);
     groupInfo.appendChild(groupTotalPassengers);
     groupInfo.appendChild(groupTotalPrice);
-    document.body.appendChild(groupInfo);
+    groupContainer.appendChild(groupInfo);
   }
 
-  displayPassengers() {
-    this.passengers.forEach((passenger) => passenger.info());
+  displayPassengers(groupContainer) {
+    let passengersInfo = document.createElement("div");
+    passengersInfo.className = `passengers-info`;
+    this.passengers.forEach((passenger) => passenger.info(passengersInfo));
+    groupContainer.appendChild(passengersInfo);
   }
 }
 
@@ -108,34 +131,60 @@ function createMultipleGroups(sizes) {
   return groups;
 }
 
-function horizontalLine(number) {
-  console.log("\x1b[33m" + "-".repeat(50) + "\x1b[0m");
-  line = document.createElement("div");
-  line.style.width = "100%";
-  line.style.height = "1px";
-  line.style.margin = "1rem 0";
-  line.style.borderBottom = "1px solid #000";
-  line.className = `horizontal-line-${number}`;
-  document.body.appendChild(line);
-}
-
-const groupSizes = [3, 1];
-const groups = createMultipleGroups(groupSizes);
-
-groups.push(createGroup(2));
-groups.push(createGroup(4));
-
 // Display Groups and Passengers
 function displayAllGroups(groups) {
+  document.querySelectorAll(".group-container").forEach((group) => group.remove());
   groups.forEach((group, index) => {
-    if (index > 0) horizontalLine(index); // Add separation between groups
-    group.groupInfo();
-    group.displayPassengers();
+    let groupContainer = document.createElement("section");
+    groupContainer.className = `group-container`;
+    group.groupInfo(groupContainer);
+    group.displayPassengers(groupContainer);
+    document.body.appendChild(groupContainer);
   });
 }
 
-groupsButton = document.getElementById("groupsButton").addEventListener("click", function() {
-  console.clear();
-  subtitle = document.getElementById("subtitle").innerHTML = "All Groups:";
+let groups = [];
+
+document.getElementById("groupsButton").addEventListener("click", function() {
+  Person.passengerCount = 0;
+  groupCounter = 0;
+
+  function generateRandomGroupSizes() {
+    const groupSizes = [];
+    let groupsAmount = document.getElementById("groupInput").value;
+    if (groupsAmount === "") groupsAmount = 1;
+    for (let i = 0; i < groupsAmount; i++) {
+      groupSizes.push(Math.floor(Math.random() * 12) + 1);
+    }
+    return groupSizes;
+  }
+
+  const groupSizes = generateRandomGroupSizes();
+  groups = createMultipleGroups(groupSizes);
   displayAllGroups(groups);
+});
+
+document.getElementById("addButton").addEventListener("click", function() {
+  let input = document.getElementById("addInput").value;
+  if (input === "") input = 1;
+  for (let i = 0; i < input ; i++) {
+    const newGroup = createGroup(Math.floor(Math.random() * 12) + 1);
+    groups.push(newGroup);
+  }
+  displayAllGroups(groups);
+});
+
+document.getElementById("locationButton").addEventListener("click", function() {
+  const randomLocation = Math.floor(Math.random() * locations.length);
+  const locationElement = document.getElementById("location");
+  locationElement.innerHTML = "";
+  locationElement.innerHTML = locations[randomLocation];
+});
+
+document.getElementById("groupInput").addEventListener("click", function(event) {
+  event.stopPropagation();
+});
+
+document.getElementById("addInput").addEventListener("click", function(event) {
+  event.stopPropagation();
 });
